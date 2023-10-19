5173db33f02dc6337592b1221ea94a2533379750
AnnKamau <3annfaithk@gmail.com>
2023-10-19 11:50:31 -0700

    stack.c

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top 
+ * @head: head stack
+ * @counter: number_line
+ * Return: none
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
