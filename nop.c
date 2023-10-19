b572d35013a20570a159a1f58e424ab07a619f1c
AnnKamau <3annfaithk@gmail.com>
2023-10-19 11:23:18 -0700

    nop.c

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * f_nop - nothing
+ * @head: head_stack
+ * @counter: number_line
+ * Return: none
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
